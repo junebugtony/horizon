8b4d51c5979166e8580b7a89676974b21868fab5
Anthony Malang <anthonymalang@u.boisestate.edu>
2024-10-22 20:26:47 -0600

    structure changes + layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,13 @@
+import './globals.css';
+import { Metadata } from 'next';
+import { Navbar } from '@/components/Navbar';
+import { Footer } from '@/components/Footer';
+
+export const metadata: Metadata = {
+  title: "Project Horizon",
+  description: "Welcome to Horizon"
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -5,8 +15,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <title>Project Horizon</title>
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        <main className='relative overflow-hidden'>
+          {children}
+        </main>
+        <Footer />
+      </body>
     </html>
   )
 }
